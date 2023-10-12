306173a277c7b60f12858a775e73898cb577eb3f
Bhavika191 <bhavika.parmar@theminimalist.in>
2023-10-12 16:28:12 +0530

    homepage

--- js/custom.js
+++ js/custom.js
@@ -2,5 +2,67 @@
 $('#toggle').click(function () {
     $(this).toggleClass('active');
     $('#overlay').toggleClass('open');
+    $('body').toggleClass('opentwo');
 });
+
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function (event) {
+    didScroll = true;
+});
+
+setInterval(function () {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+
+// header sticky on scroll up js
+function hasScrolled() {
+    var st = $(this).scrollTop();
+
+    // Make sure they scroll more than delta
+    if (Math.abs(lastScrollTop - st) <= delta)
+        return;
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight) {
+        // Scroll Down
+        $('header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if (st + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+
+    lastScrollTop = st;
+}
+
 // header menu code ends
+
+
+// our partner slider starts
+$('.logoSlider').slick({
+    dots: false,
+    arrows: false,
+    slidesToShow: 4,
+    autoplay: true,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 920,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+    ]
+  });
+// our partber slider ends
