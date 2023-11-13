9a1490b41219e52ed64692c00e303d9a1bc38020
Bhavika191 <bhavika.parmar@theminimalist.in>
2023-11-13 19:00:29 +0530

    form css

--- js/custom.js
+++ js/custom.js
@@ -5,6 +5,46 @@ $('#toggle').click(function () {
   $('#overlay').toggleClass('open');
   $('body').toggleClass('opentwo');
 });
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function (event) {
+  didScroll = true;
+});
+
+setInterval(function () {
+  if (didScroll) {
+    hasScrolled();
+    didScroll = false;
+  }
+}, 250);
+
+
+// header sticky on scroll up js
+function hasScrolled() {
+  var st = $(this).scrollTop();
+
+  // Make sure they scroll more than delta
+  if (Math.abs(lastScrollTop - st) <= delta)
+    return;
+  // If they scrolled down and are past the navbar, add class .nav-up.
+  // This is necessary so you never see what is "behind" the navbar.
+  if (st > lastScrollTop && st > navbarHeight) {
+    // Scroll Down
+    $('header').removeClass('nav-down').addClass('nav-up');
+  } else {
+    // Scroll Up
+    if (st + $(window).height() < $(document).height()) {
+      $('header').removeClass('nav-up').addClass('nav-down');
+    }
+  }
+
+  lastScrollTop = st;
+}
 // header menu code ends
 
 //---------------------------------------------------------------------- Shamal's Js Code Starts ---------------------------------------------------------------//
@@ -120,49 +160,6 @@ jQuery(function ($) {
 //Offset code ends
 
 //------------------------------------------------------------------- Shamal's Js Code ends ---------------------------------------------------------------------//
-
-
-// Hide Header on on scroll down
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $('header').outerHeight();
-
-$(window).scroll(function (event) {
-  didScroll = true;
-});
-
-setInterval(function () {
-  if (didScroll) {
-    hasScrolled();
-    didScroll = false;
-  }
-}, 250);
-
-
-// header sticky on scroll up js
-function hasScrolled() {
-  var st = $(this).scrollTop();
-
-  // Make sure they scroll more than delta
-  if (Math.abs(lastScrollTop - st) <= delta)
-    return;
-  // If they scrolled down and are past the navbar, add class .nav-up.
-  // This is necessary so you never see what is "behind" the navbar.
-  if (st > lastScrollTop && st > navbarHeight) {
-    // Scroll Down
-    $('header').removeClass('nav-down').addClass('nav-up');
-  } else {
-    // Scroll Up
-    if (st + $(window).height() < $(document).height()) {
-      $('header').removeClass('nav-up').addClass('nav-down');
-    }
-  }
-
-  lastScrollTop = st;
-}
-
-// header menu code ends
 
 
 
