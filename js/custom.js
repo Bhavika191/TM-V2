609331f111bf90e5b804f98c31134bf353127307
sdurbule27 <shamal.durbule@theminimalist.in>
2023-11-17 15:24:30 +0530

    career page

--- js/custom.js
+++ js/custom.js
@@ -58,44 +58,46 @@ jQuery(document).ready(function () {
     });
   }
 });
-
 //Service page read more and Read less code ends
 
 //Service page active menu navbar starts
 $(document).ready(function () {
   let menuScrollTimer = null;
   $(".filter-wrapper a").click(function (e) {
-    // Prevent default behaviour ( scroll to element )
-    e.preventDefault();
-    if (menuScrollTimer === null) {
-      // Highlight the clicked item
-      $('.filter-wrapper a.active').removeClass('active');
-      $(this).addClass('active');
-      // Smooth scroll to the target section
-      let target = $(this).attr('href');
-      $('html, body').animate({ scrollTop: $(target).offset().top - 250 }, 1050);
-      // Set `menuScrollTimer` timer
-      // This will prevents multiple clicks on menu items whule the smooth scroll is taking effect
-      // This will also prevent the scroll logic from running
-      menuScrollTimer = setTimeout(function () {
-        clearTimeout(menuScrollTimer);
-        menuScrollTimer = null;
-      }, 1050);
-    }
+      // Prevent default behaviour ( scroll to element )
+      e.preventDefault();
+      $('.service-container .filterSec').animate({
+        scrollLeft: "+=200px"
+      }, "slow");
+      if (menuScrollTimer === null) {
+          // Highlight the clicked item
+          $('.filter-wrapper a.active').removeClass('active');
+          $(this).addClass('active');
+          // Smooth scroll to the target section
+          let target = $(this).attr('href');
+          $('html, body').animate({ scrollTop: $(target).offset().top - 250}, 1050);
+          // Set `menuScrollTimer` timer
+          // This will prevents multiple clicks on menu items whule the smooth scroll is taking effect
+          // This will also prevent the scroll logic from running
+          menuScrollTimer = setTimeout(function () {
+              clearTimeout(menuScrollTimer);
+              menuScrollTimer = null;
+          }, 1050);
+      }
   });
 
   $(window).scroll(function (e) {
-    // Avoid triggering the logic if the scroll event is triggerd from clicking one of the items
-    if (menuScrollTimer === null) {
-      let windowTop = $(this).scrollTop();
+      // Avoid triggering the logic if the scroll event is triggerd from clicking one of the items
+      if (menuScrollTimer === null) {
+          let windowTop = $(this).scrollTop();
 
-      $('.filter-wrapper a').each(function (event) {
-        if (windowTop >= $($(this).attr('href')).offset().top - 250) {
-          $('.filter-wrapper .active').removeClass('active');
-          $(this).addClass('active');
-        }
-      });
-    }
+          $('.filter-wrapper a').each(function (event) {
+              if (windowTop >= $($(this).attr('href')).offset().top - 250) {
+                  $('.filter-wrapper .active').removeClass('active');
+                  $(this).addClass('active');
+              }
+          });
+      }
   });
 });
 //Service page active menu navbar ends
