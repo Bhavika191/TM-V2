9c176b5a31474445249c87a5e7e88d59c31c8af8
sdurbule27 <shamal.durbule@theminimalist.in>
2023-11-17 16:55:05 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -91,6 +91,7 @@ jQuery(document).ready(function () {
   }//for loop closed
 });
 //Service page read more and Read less code ends
+
 //Service page active menu navbar starts
 $(document).ready(function () {
 let menuScrollTimer = null;
@@ -130,6 +131,7 @@ $(window).scroll(function (e) {
 });
 });
 //Service page active menu navbar ends
+
 //Off set code starts
 jQuery(function ($) {
 $('a[href*="#"]:not([href="#"])').click(function () {
@@ -149,43 +151,7 @@ $(window).on('load', function () {
 });
 //Offset code ends
 //------------------------------------------------------- Shamal's Code ends --------------------------------------------------//
-// Hide Header on on scroll down
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $('header').outerHeight();
-$(window).scroll(function (event) {
-didScroll = true;
-});
-setInterval(function () {
-if (didScroll) {
-    hasScrolled();
-    didScroll = false;
-}
-}, 250);
-// header sticky on scroll up js
-function hasScrolled() {
-var st = $(this).scrollTop();
-// Make sure they scroll more than delta
-if (Math.abs(lastScrollTop - st) <= delta)
-  return;
-// If they scrolled down and are past the navbar, add class .nav-up.
-// This is necessary so you never see what is "behind" the navbar.
-if (st > lastScrollTop && st > navbarHeight) {
-  // Scroll Down
-  $('header').removeClass('nav-down').addClass('nav-up');
-  $('.service-container .filterSec').animate({
-    scrollLeft: $('.filter-wrapper a.active').offset().left - $('.filter-wrapper').offset().left
-  }, "slow");
-} else {
-  // Scroll Up
-  $('header').removeClass('nav-up').addClass('nav-down');
-  if (st + $(window).height() < $(document).height()) {
-    $('header').removeClass('nav-up').addClass('nav-down');
-}
-}
-lastScrollTop = st;
-}
+
 
 // our partner slider starts
 $('.logoSlider').slick({
